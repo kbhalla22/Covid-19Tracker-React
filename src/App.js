fce9e5e0222bf9469feb28543547474ba14a15df
karanbhalla <kbhalla22@gmail.com>
2020-04-12 11:51:05 -0400

    added self closing components

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React,{ Component} from 'react';
-import Cards from './components/Cards/Cards';
-import Chart from './components/Chart/Chart'; 
-import CountryPicker from './components/CountryPicker/CountryPicker';
+import {Cards,Chart,CountryPicker} from './components';
 
 
 class App extends Component{
     render(){
         return (
             <div>
-                <h1>App</h1>
+              <Cards/>
+              <Chart/>
+              <CountryPicker/>
             </div>
         )
     }
