92466eeaf32f343efaffda02433e41110478b0bd
karanbhalla <kbhalla22@gmail.com>
2020-04-12 11:44:45 -0400

    Added basic div to them

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
-import React,{ Component} from 'react'
+import React,{ Component} from 'react';
+import Cards from './components/Cards/Cards';
+import Chart from './components/Chart/Chart'; 
+import CountryPicker from './components/CountryPicker/CountryPicker';
 
 
 class App extends Component{
