c729e14667535813e0ebfbcdc7b680e8929608c9
karanbhalla <kbhalla22@gmail.com>
2020-04-12 18:30:44 -0400

    setting up the data from the countries

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,8 +1,26 @@
-import React from 'react';
-
+import React,{useState,useEffect} from 'react';
+import {NativeSelect,FormControl} from '@material-ui/core';
+import styles from './CountryPicker.module.css';
+import {countries} from '../../api';
+/**
+ * Will take a useeffect which takes a callback.
+ * inside that we create an asynchronous function
+ * },[setFetchedCountries. added a second parameter to show that it will only change when 
+ */
 const CountryPicker=()=>{
+    const [fetchedCountries,setFetchedCountries]=useState([]);
+    useEffect(()=>{
+const fetchCountries=async()=>{
+    setFetchedCountries(await countries);
+}
+fetchCountries();
+    },[setFetchedCountries]);
     return (
-        <h1>CountryPicker</h1>
+       <FormControl className={styles.formControl}>
+           <NativeSelect>
+<option value="global">Global</option>
+           </NativeSelect>
+       </FormControl>
     )
 }
 export default CountryPicker;
