c849940145cb517f2cc873063e8ac20b8df6bd03
karanbhalla <kbhalla22@gmail.com>
2020-04-12 12:19:30 -0400

    returning response

--- src/api/index.js
+++ src/api/index.js
@@ -2,9 +2,10 @@ import axios from 'axios';
 
 const url= 'http://covid19.mathdro.id/api';
 
-const fetchData=async()=>{
+export const fetchData=async()=>{
 try{
 const response=await axios.get(url);
+return response;
 }
 catch(error){
 
