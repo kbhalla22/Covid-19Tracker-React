080bd85152487285d42f722eb5836878352c7874
karanbhalla <kbhalla22@gmail.com>
2020-04-12 18:30:17 -0400

    fetching the name of the countries

--- src/api/index.js
+++ src/api/index.js
@@ -4,24 +4,32 @@ const url = "http://covid19.mathdro.id/api";
 
 export const fetchData = async () => {
   try {
-    const{ data: { confirmed, recovered, deaths, lastUpdate }} = await axios.get(url);
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate }
+    } = await axios.get(url);
     return { confirmed, recovered, deaths, lastUpdate };
   } catch (error) {}
-}
+};
 /**
  * {data}acts like an array
  * loop over data array and show only the relevant one.We can map over it
  */
-export const fetchDailyData = async()=>{
-  try{
-const {data}= await axios.get(`${url}/daily`);
-const modifiedData =data.map((dailyData)=>({
-  confirmed: dailyData.confirmed.total,
-  deaths: dailyData.deaths.total,
-  date: dailyData.reportDate,
-}));
-return modifiedData;
-  }catch(error){
+export const fetchDailyData = async () => {
+  try {
+    const { data } = await axios.get(`${url}/daily`);
+    const modifiedData = data.map(dailyData => ({
+      confirmed: dailyData.confirmed.total,
+      deaths: dailyData.deaths.total,
+      date: dailyData.reportDate
+    }));
+    return modifiedData;
+  } catch (error) {console.log(error);}
+};
 
-  }
-}
+export const countries = async () => {
+  try {
+    const response= await axios.get(`${url}/countries`);
+    console.log(response);
+    
+  } catch (error) {console.log(error);}
+};
