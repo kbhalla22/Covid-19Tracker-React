b5f415a09b82e6b89f10e3acc46f86a0a2b09335
karanbhalla <kbhalla22@gmail.com>
2020-04-12 15:24:10 -0400

    Applied the changes to other cards

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,5 +1,9 @@
 import React from "react";
 import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+/**
+ * this import keeps on changing the count of the numbers that are being fetched
+ */
+import CountUp from 'react-countup';
 
 import styles from "./Cards.modules.css";
 //pass props to card(acc to app.js)
@@ -16,6 +20,8 @@ const Cards = ({data:{confirmed,recovered,deaths,lastUpdate}}) => {
      * Typography is basically used for styling
      * Gutters are spaces in css grids
      * gutterBottom provides padding on the bottom.
+     * add a self closing component called countup
+     * new Date.toDateString returns it in a human readavle format
      */
     <div className={styles.container}>
       <Grid container spacing={3} justify="center">
@@ -24,8 +30,15 @@ const Cards = ({data:{confirmed,recovered,deaths,lastUpdate}}) => {
             <Typography color="textSecondary" gutterBottom>
               Infected
             </Typography>
-            <Typography variant="h5">{confirmed.value}</Typography>
-            <Typography color="textSecondary" >REAL DATE</Typography>
+            <Typography variant="h5">
+            <CountUp
+            start={0}
+            end={confirmed.value}
+            duration={2.5}
+            separator=","
+            />
+            </Typography>
+            <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="body2" >Number of active cases of COVID-19</Typography>
           </CardContent>
         </Grid>
@@ -36,8 +49,15 @@ const Cards = ({data:{confirmed,recovered,deaths,lastUpdate}}) => {
             <Typography color="textSecondary" gutterBottom>
               Recovered
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="textSecondary" >REAL DATE</Typography>
+            <Typography variant="h5">
+            <CountUp
+            start={0}
+            end={recovered.value}
+            duration={2.5}
+            separator=","
+            />
+            </Typography>
+            <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="body2" >Number of recoveries COVID-19</Typography>
           </CardContent>
         </Grid>
@@ -48,8 +68,15 @@ const Cards = ({data:{confirmed,recovered,deaths,lastUpdate}}) => {
             <Typography color="textSecondary" gutterBottom>
               Deaths
             </Typography>
-            <Typography variant="h5">REAL DATA</Typography>
-            <Typography color="textSecondary" >REAL DATE</Typography>
+            <Typography variant="h5">
+            <CountUp
+            start={0}
+            end={deaths.value}
+            duration={2.5}
+            separator=","
+            />
+            </Typography>
+            <Typography color="textSecondary" >{new Date(lastUpdate).toDateString()}</Typography>
             <Typography variant="body2" >Number of deaths caused by COVID-19</Typography>
           </CardContent>
         </Grid>
