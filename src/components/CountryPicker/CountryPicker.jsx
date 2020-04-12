9a43fbedf83281a6fd52ed61a8f2c34cc6cf5b09
karanbhalla <kbhalla22@gmail.com>
2020-04-12 18:41:52 -0400

    fetching the countries from index.js

--- src/components/CountryPicker/CountryPicker.jsx
+++ src/components/CountryPicker/CountryPicker.jsx
@@ -1,26 +1,28 @@
-import React,{useState,useEffect} from 'react';
-import {NativeSelect,FormControl} from '@material-ui/core';
-import styles from './CountryPicker.module.css';
-import {countries} from '../../api';
+import React, { useState, useEffect } from "react";
+import { NativeSelect, FormControl } from "@material-ui/core";
+import styles from "./CountryPicker.module.css";
+import { fetchCountries } from "../../api";
 /**
  * Will take a useeffect which takes a callback.
  * inside that we create an asynchronous function
- * },[setFetchedCountries. added a second parameter to show that it will only change when 
+ * },[setFetchedCountries. added a second parameter to show that it will only change when
  */
-const CountryPicker=()=>{
-    const [fetchedCountries,setFetchedCountries]=useState([]);
-    useEffect(()=>{
-const fetchCountries=async()=>{
-    setFetchedCountries(await countries);
-}
-fetchCountries();
-    },[setFetchedCountries]);
-    return (
-       <FormControl className={styles.formControl}>
-           <NativeSelect>
-<option value="global">Global</option>
-           </NativeSelect>
-       </FormControl>
-    )
-}
+const CountryPicker = () => {
+  const [fetchedCountries, setFetchedCountries] = useState([]);
+  useEffect(() => {
+    const fetchAPI = async () => {
+      setFetchedCountries(await fetchCountries());
+    };
+   
+    fetchAPI();
+  }, [setFetchedCountries]);
+  console.log(fetchedCountries);
+  return (
+    <FormControl className={styles.formControl}>
+      <NativeSelect>
+        <option value="global">Global</option>
+      </NativeSelect>
+    </FormControl>
+  );
+};
 export default CountryPicker;
