c0874cc366a0a01d1f08da762bd54b40657e0add
karanbhalla <kbhalla22@gmail.com>
2020-04-12 14:53:32 -0400

    Adding a card grid to the page

--- src/components/Cards/Cards.jsx
+++ src/components/Cards/Cards.jsx
@@ -1,9 +1,33 @@
-import React from 'react';
+import React from "react";
+import { Card, CardContent, Typography, Grid } from "@material-ui/core";
+
+import styles from "./Cards.modules.css";
 //pass props to card(acc to app.js)
-const Cards=(props)=>{
-    console.log(props);
-    return (
-        <h1>Cards</h1>
-    )
-}
+const Cards = (props) => {
+  console.log(props);
+  return (
+    /**
+     * this div acts as a container
+     * Grid acts like a container for all the cards.
+     * Then add another grid(item)
+     * Typography is basically used for styling
+     * Gutters are spaces in css grids
+     * gutterBottom provides padding on the bottom.
+     */
+    <div className={styles.container}>
+      <Grid container spacing={3} justify="center">
+        <Grid item component={Card}>
+          <CardContent>
+            <Typography color="textSecondary" gutterBottom>
+              Infected
+            </Typography>
+            <Typography variant="h5">REAL DATA</Typography>
+            <Typography color="textSecondary" >REAL DATE</Typography>
+            <Typography variant="body2" >Number of active cases of COVID-19</Typography>
+          </CardContent>
+        </Grid>
+      </Grid>
+    </div>
+  );
+};
 export default Cards;
