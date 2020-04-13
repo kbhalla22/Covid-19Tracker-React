a0aa2edf91e8b59ca2d2553fef5a4882a1a04b40
karanbhalla <kbhalla22@gmail.com>
2020-04-12 22:21:09 -0400

    adding image ans styles for image

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React,{ Component} from 'react';
 import {Cards,Chart,CountryPicker} from './components';
 import styles from './App.module.css';
 import {fetchData} from './api';
+import coronaImage from './images/CoronaVirusHeader.jpg';
 
 /**
  * Need to have country in the state of app.js because it is the parent component of all the components.
@@ -33,7 +34,8 @@ class App extends Component{
 
 //pass data as props to card component
         return (
-            <div className={styles.ccontainer}>
+            <div className={styles.container}>
+<img className={styles.image }src={coronaImage} alt="COVID-19"/>
             
               <Cards data={data}/>
               <CountryPicker handleCountryChange={this.handleCountryChange}/>
